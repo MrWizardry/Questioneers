e798d1821d51a1df0299e46c86dc9d1283ff74c4
PedrinDev <pedro.silva25@estudante.ifms.edu.br>
2023-02-16 02:02:45 -0400

    coisas

--- Assets/Scripts/Botoes_Menu_Inicio.cs
+++ Assets/Scripts/Botoes_Menu_Inicio.cs
@@ -5,18 +5,28 @@ using UnityEngine.UIElements;
 
 public class Botoes_Menu_Inicio : MonoBehaviour
 {
+
     public Button Start;
     public Button Config;
     public Button Quit;
+    public Button Beck;
+    public Toggle Res1;
+    public Toggle Res2;
     public VisualElement MenuIni;
     public VisualElement MenuCon;
 
-    private void Awake() {
+
+    private void Awake() 
+    {
         var root = GetComponent<UIDocument>();
 
         Start = root.rootVisualElement.Q<Button>("Start");
         Config = root.rootVisualElement.Q<Button>("Config");
         Quit = root.rootVisualElement.Q<Button>("Sair");
+        Beck = root.rootVisualElement.Q<Button>("Voltar");
+
+        Res1 = root.rootVisualElement.Q<Toggle>("19x10");
+        Res2 = root.rootVisualElement.Q<Toggle>("12x78");
 
         MenuIni = root.rootVisualElement.Q<VisualElement>("Menu");
         MenuCon = root.rootVisualElement.Q<VisualElement>("Configu");
@@ -24,8 +34,10 @@ public class Botoes_Menu_Inicio : MonoBehaviour
         Start.clicked += StartPress;
         Config.clicked += ConfigPress;
         Quit.clicked += QuitPress;
+        Beck.clicked += BeckPress;
+        Res1.focusable = Res1Focus;
+        Res2.focusable = Res2Focus;
     }
-
     void StartPress()
     {
         Debug.Log("Começou o Jogo");
@@ -33,11 +45,38 @@ public class Botoes_Menu_Inicio : MonoBehaviour
 
     void ConfigPress()
     {
+        MenuIni.style.display = DisplayStyle.None;
+        MenuCon.style.display = DisplayStyle.Flex;
         Debug.Log("Abriu as Configs");
     }
 
     void QuitPress()
     {
         Debug.Log("Saiu do Jogo");
+    }
+
+    void BeckPress()
+    {  
+        MenuIni.style.display = DisplayStyle.Flex;
+        MenuCon.style.display = DisplayStyle.None;
+    }
+
+    void Res1Focus() 
+    {   
+        Screen.SetResolution(1920,1080, true);
+        
+        if(Res1.focusable == true )
+        {
+            Res1.focusable = true;
+        }
+    }
+
+    void Res2Focus()
+    {
+        Screen.SetResolution(1280,720, true);
+        if(Res2.focusable == false)
+        {
+            Res2.focusable = true;
+        }
     }
 }
